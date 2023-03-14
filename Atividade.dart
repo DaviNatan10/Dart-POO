141549aa1cd84da038942892d80e5efef1b01c86
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2023-03-14 18:23:38 -0300

    Tarefa 1 Concluida.

--- Atividade.dart
+++ Atividade.dart
@@ -1,5 +1,48 @@
 void main() {
-  for (int i = 0; i < 10; i++) {
-    print('hello ${i + 1}');
-  }
+   Produto Cana = Produto(nome: "Cana", valor: 15, validade: "12/12/2023");
+  Produto Carne = Produto(nome: "Carne", valor: 35, validade: "12/12/2023");
+  
+
+    Item cerveja = new Item(quantidade:5,produto:Cana);
+    Item  carne = new Item(quantidade:4,produto: Carne);
+
+   Venda venda = Venda(data: "13/03/2023", itens: [cerveja, carne, ]);
+    print("O total da venda é: R\$${venda.total()}");
+
+    
+
+
+    
+  
+}
+
+class Venda {
+  String data;
+  List<Item> itens = [];
+  double total() => itens.fold(0, (sum, e) => sum + e.total());
+  Venda({required this.data, required this.itens});
+
+  void adicionarItem(Item item){
+        itens.add(item);
+    }
+}
+
+
+
+
+
+class Produto{
+   String nome;
+   double valor;
+   String validade; 
+   Produto({required this.nome, required this.valor, required this.validade});
+
+}
+
+class Item{
+  double quantidade;
+  Produto produto;
+
+  double total() => quantidade * produto.valor;
+  Item({required this.quantidade, required this.produto});
 }
