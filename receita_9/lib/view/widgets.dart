cbac2005f8843c5d8b4ca158a6bdf6047f440908
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2023-06-18 23:33:40 -0300

    Questao 5

--- receita_9/lib/view/widgets.dart
+++ receita_9/lib/view/widgets.dart
@@ -88,23 +88,33 @@ class DataTableWidget extends StatelessWidget {
   final List jsonObjects;
   final List<String> columnNames;
   final List<String> propertyNames;
-  DataTableWidget( {this.jsonObjects = const [], this.columnNames = const [], this.propertyNames= const []});
+
+  DataTableWidget({this.jsonObjects = const [], this.columnNames = const [], this.propertyNames = const []});
+
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: columnNames.map( 
-                (name) => DataColumn(
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: DataTable(
+        columns: columnNames
+            .map((name) => DataColumn(
                   label: Expanded(
-                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
-                  )
-                )
-              ).toList(),
-      rows: jsonObjects.map( 
-        (obj) => DataRow(
-            cells: propertyNames.map(
-              (propName) => DataCell(Text(obj[propName]))
-            ).toList()
-          )
-        ).toList());
+                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic)),
+                  ),
+                ))
+            .toList(),
+        rows: jsonObjects
+            .map(
+              (obj) => DataRow(
+                cells: propertyNames
+                    .map(
+                      (propName) => DataCell(Text(obj[propName])),
+                    )
+                    .toList(),
+              ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
